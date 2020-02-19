0f161af0e9e2250bf9bcae34bba3f4ff1956e5e7
Jessica Shu <jessicashu7@gmail.com>
2020-02-19 10:41:31 -0800

    added stride result activity

---
+++ app/src/main/java/com/example/stridon/StrideResultActivity.java
@@ -0,0 +1,14 @@
+package com.example.stridon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StrideResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stride_result);
+    }
+}
